7f41378ba5307ac9d641930d923bacfdb0ca6f29
Kevin Tavares <kvatavares30@icloud.com>
2023-06-21 16:18:15 -0300

    form part one

---
+++ src/MyComponents/MyForm.tsx
@@ -0,0 +1,19 @@
+
+
+const MyForm = () => {
+    return (
+    <form>
+         <label>
+               Nome:
+               <input type="text" name="name" />
+               <br></br>
+               Cidade:
+               <input type="text" name="name" />
+        </label>
+        <br />
+               <input type="submit" value="Enviar" />
+    </form>
+        
+)};
+
+export default MyForm;
